4eb2f7518977a627fc5500a3b6a86d79f7e5c3da
Wilfreschu <Wschuchner@udesa.edu.ar>
2025-03-24 17:36:51 -0300

    vinculacion en app

--- TpIntegrador2/app.js
+++ TpIntegrador2/app.js
@@ -6,6 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const headerRouter = require("./routes/headerLogueado")
+const loginRouter = require("./routes/login")
+const productAddRouter = require("./routes/product-add")
+const productRouter = require("./routes/product")
+const profileRouter = require("./routes/profile")
+const registerRouter = require("./routes/register")
+const searchRouter = require("./routes/search-results")
+
 
 var app = express();
 
@@ -21,6 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/header", headerRouter)
+app.use('/login', loginRouter);
+app.use('/productAdd', productAddRouter);
+app.use('/product', productRouter);
+app.use('/profile', profileRouter);
+app.use('/register', registerRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
